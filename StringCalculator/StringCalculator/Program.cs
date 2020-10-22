e5f6011f1dca016ea9a4caebda12a084978dfe85
blueboy0904 <73291298+blueboy0904@users.noreply.github.com>
2020-10-22 14:28:12 +0200

    Add files via upload

---
+++ StringCalculator/StringCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StringCalculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Input: ");
+			var input = Console.ReadLine();
+			var total = 0;
+
+			if (string.IsNullOrWhiteSpace(input))
+			{
+				Console.WriteLine("0");
+			}
+			else
+			{
+				var numbers = input.Split(new string[] { ",", "\\n" }, StringSplitOptions.None);
+
+				foreach (var number in numbers)
+				{
+					total = total + Convert.ToInt32(number);
+				}
+
+				Console.WriteLine(total);
+			}
+		}
+	}
+}
